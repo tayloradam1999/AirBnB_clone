5aec95835fc002cbcce2fa222a8fd75519d567f8
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-19 20:58:58 +0000

    Added tests for base_model class

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -3,25 +3,46 @@
     Unit test module for BaseModel class
 """
 import unittest
+import os
+from models.engine.file_storage import FileStorage
+from datetime import datetime
 from models.base_model import BaseModel
 
 
-class TestBaseModelMethods(unittest.TestCase):
+my_model = BaseModel()
 
+
+class TestBaseModelMethods(unittest.TestCase):
     """ Class for BaseModel tests """
 
-    def test_task_3(self):
-        b1 = BaseModel()
-        b1.name = "Holberton"
-        self.assertEqual(b1.name, "Holberton")
-        b1.my_number = 89
-        self.assertEqual(b1.my_number, 89)
-        print(b1)
-        b1_json = b1.to_dict()
-        print(b1_json)
-        print("JSON of b1:")
-        for k in b1_json.keys():
-            print("\t{}: ({}) - {}".format(k, type(b1_json[k]), b1_json[k]))
+    def test_id(self):
+        """ Unittesting test_id instance """
+        self.assertTrue(type(my_model.id) is str)
+
+    def test_created_at(self):
+        """ Unittesting created_at instance """
+        self.assertTrue(type(my_model.created_at) is datetime)
+
+    def test_str(self):
+        """ Unittesting __str__ method """
+        self.assertEqual(str(my_model), "[{}] ({}) {}".format(
+            my_model.__class__.__name__, my_model.id, my_model.to_dict()))
+    
+    def test_to_dict(self):
+        """ Unittesting to_dict method """
+        my_dict = my_model.to_dict()
+        self.assertTrue(type(my_dict["created_at"]) is str)
+        self.assertTrue(type(my_dict) is dict)
+        self.assertEqual(my_dict["__class__"], "BaseModel")
+        self.assertIn("__class__", my_dict.keys())
+   
+    def test_save(self):
+        """ Unittesting save method """
+        my_save = my_model.updated_at
+        my_model.save()
+        my_new_save = my_model.updated_at
+        self.assertNotEqual(my_save, my_new_save)
+
 
 if __name__ == '__main__':
     unittest.main()
