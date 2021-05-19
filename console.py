968f448e6dfdf15521962097fbd4dce01314fcf5
Adam Brimer <acbrimer@github.com>
2021-05-19 00:01:33 +0000

    Added template for console

---
+++ console.py
@@ -0,0 +1,34 @@
+import cmd, sys
+
+class AirbnbShell(cmd.Cmd):
+    intro = "Welcome to the turtle shell.   Type help or ? to list commands.\n"
+    prompt = "(hbnb) "
+    file = None
+
+    def do_hello(self, arg):
+        """ Print 'Hello', followed by arg: HELLO HOLBERTON """
+        print("Hello, {}".format(arg))
+    
+    def do_bye(self, arg):
+        'Stop recording, close the hbnb window, and exit:  BYE'
+        print('Thank you for using hbnb')
+        self.close()
+        return True
+
+    def precmd(self, line):
+        line = line.lower()
+        if self.file and 'playback' not in line:
+            print(line, file=self.file)
+        return line
+    def close(self):
+        if self.file:
+            self.file.close()
+            self.file = None
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+    return tuple(map(int, arg.split()))
+
+if __name__ == '__main__':
+    AirbnbShell().cmdloop()
+
