f58e907b37597a82c08e639acc0ce1b1fa594e76
tayloradam1999 <tayloradam1999@gmail.com>
2021-05-19 19:39:54 +0000

    Pushing new base_models file

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,22 @@
 """ Module for BaseModel class """
 import uuid
 from datetime import datetime
-import storage
+import models
 
 class BaseModel:
 
     """ BaseModel class """
 
     def __init__(self, *args, **kwargs):
-        self.__id = str(uuid.uuid4())
-        self.__created_at = datetime.now()
-        self.__updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.new(self)
         if kwargs is not None:
             if "__class__" in kwargs.keys():
                 del kwargs["__class__"]
             if "__created_at" in kwargs.keys():
-                del kwargs["__created_at"]
+                del kwargs["created_at"]
             if "id" in kwargs.keys():
                 self.__id = kwargs["id"]
                 del kwargs["id"]
@@ -40,29 +41,29 @@ class BaseModel:
     def formatDate(date):
         return str(date.strftime("%Y-%m-%dT%H:%M:%S.%f"))
 
-    @property
+    """@property
     def id(self):
-        return self.__id
+        return self.id
 
     @id.setter
     def id(self, value):
-        self.__id = value
+        self.id = value
 
     @property
     def created_at(self):
-        return self.__created_at
+        return self.created_at
 
     @property
     def updated_at(self):
-        return self.__updated_at
+        return self.updated_at
 
     @updated_at.setter
     def updated_at(self, value):
-        self.__updated_at = value
+        self.updated_at = value"""
 
     def save(self):
-        self.__updated_at = datetime.now()
-        storage.save(self)
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         d = dict(self.__dict__)
