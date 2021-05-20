3760b192317a4224bbb8863fb6cbdb161acb1f08
Adam Brimer <acbrimer@github.com>
2021-05-20 22:32:36 +0000

    Fix for task 5

--- models/base_model.py
+++ models/base_model.py
@@ -4,65 +4,64 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
 
     """ BaseModel class """
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+
+        uid = str(uuid.uuid4())
+        self.id = uid if "id" not in kwargs else kwargs["id"]
+        n = datetime.now()
+        self.created_at = n if "created_at" not in kwargs else kwargs[
+            "created_at"]
+        self.updated_at = n if "updated_at" not in kwargs else kwargs[
+            "updated_at"]
         if kwargs is not None:
             if "__class__" in kwargs.keys():
                 del kwargs["__class__"]
-            if "__created_at" in kwargs.keys():
-                del kwargs["created_at"]
             if "id" in kwargs.keys():
                 self.__id = kwargs["id"]
                 del kwargs["id"]
-            for k in kwargs:
-                if BaseModel.checkDate(kwargs[k]):
-                    kwargs[k] = datetime.strptime(
-                        kwargs[k],
-                        "%Y-%m-%dT%H:%M:%S.%f")
             self.__dict__.update(kwargs)
+        models.storage.new(self)
 
-    @staticmethod
-    def checkDate(string):
-        """ Checks if string is a date """
-        try:
-            datetime.strptime(string, "%Y-%m-%dT%H:%M:%S.%f")
-            return True
-        except ValueError:
-            return False
-
-    @staticmethod
-    def formatDate(date):
-        return str(date.strftime("%Y-%m-%dT%H:%M:%S.%f"))
-
-    """@property
+    @property
     def id(self):
-        return self.id
+        return self.__id
 
     @id.setter
     def id(self, value):
-        self.id = value
+        self.__id = value
 
     @property
     def created_at(self):
-        return self.created_at
+        return self.__created_at
+
+    @created_at.setter
+    def created_at(self, value):
+        if isinstance(value, str):
+            self.__created_at = datetime.strptime(
+                value, "%Y-%m-%d %H:%M:%S.%f")
+        else:
+            self.__created_at = value
 
     @property
     def updated_at(self):
-        return self.updated_at
+        return self.__updated_at
 
     @updated_at.setter
     def updated_at(self, value):
-        self.updated_at = value"""
+        if isinstance(value, str):
+            self.__updated_at = datetime.strptime(
+                value, "%Y-%m-%d %H:%M:%S.%f")
+        else:
+            self.__updated_at = value
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
     Module for FileStorage class
     JSON -> file serialization and file -> JSON deserialization
 """
@@ -10,9 +10,10 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
+
     """ Manages read/write and serialization for JSON file store """
-    __file_path ="HBNB_jsonfile.json"
-    __objects = dict() #keyed with <class name>.id
+    __file_path = "HBNB_jsonfile.json"
+    __objects = dict()  # keyed with <class name>.id
 
     def all(self):
         """ Returns dict of __objects """
@@ -21,16 +22,24 @@ class FileStorage:
     def new(self, obj):
         """ Add a new object to __objects """
         k = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[k] = obj.to_dict()
+        if k not in FileStorage.__objects.keys():
+            FileStorage.__objects[k] = obj
 
     def save(self):
         """ Serialize objects to JSON file in __file_path """
+        d = dict()
+        for k in FileStorage.__objects.keys():
+            d[k] = FileStorage.__objects[k].to_dict()
         with open(FileStorage.__file_path, 'w') as fp:
-            json.dump(FileStorage.__objects, fp)
+            json.dump(d, fp)
 
     def reload(self):
         """ Deserializes the JSON file to __objects (if file exists) """
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as fp:
-                FileStorage.__objects = json.load(fp)
-
+                objs = json.load(fp)
+                for k in objs.keys():
+                    # insert to __objects if not exists
+                    if k not in FileStorage.__objects.keys():
+                        obj_data = objs[k]
+                        FileStorage.__objects[k] = BaseModel(**obj_data)
