acb98354459f384f7224b651434d937f47f87a90
Adam Brimer <acbrimer@github.com>
2021-05-22 19:06:32 +0000

    Added User module

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-    Module for Users model
+    Module for User model
 """
 from models.base_model import BaseModel
 
 
-class Users(BaseModel):
+class User(BaseModel):
 
     """
         Info on AirBnB Users
@@ -15,4 +15,38 @@ class Users(BaseModel):
             first_name: string - empty string
             last_name: string - empty string
     """
-    pass
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        self.__email = str(value)
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        self.__password = str(value)
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        self.__first_name = str(value)
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        self.__last_name = str(value)
