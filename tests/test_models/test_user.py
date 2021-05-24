7c739fbcb945ec59def92144e3920dd4ef13cc5c
Adam Brimer <acbrimer@github.com>
2021-05-24 15:14:35 +0000

    Added remaining User tests for task 8

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -24,3 +24,15 @@ class TestUser(unittest.TestCase):
         """ Make sure email is string """
         self.assertTrue(isinstance(User.email, str))
         self.assertTrue(isinstance(user1.email, str))
+
+    def test_password(self):
+        self.assertTrue(isinstance(User.password, str))
+        self.assertTrue(isinstance(user1.password, str))
+
+    def test_first_name(self):
+        self.assertTrue(isinstance(User.first_name, str))
+        self.assertTrue(isinstance(user1.first_name, str))
+
+    def test_last_name(self):
+        self.assertTrue(isinstance(User.last_name, str))
+        self.assertTrue(isinstance(user1.last_name, str))
